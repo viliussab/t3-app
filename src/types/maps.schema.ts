3ebfeff95830409c301215c4e615fa7c5e189d1a
Vilius Sabonis <viliussab@github.com>
2022-09-29 23:46:09 +0300

    initial setups

---
+++ src/types/maps.schema.ts
@@ -0,0 +1,13 @@
+import z from 'zod'
+
+export const mapCreateSchema = z.object({
+  locationName: z.string(),
+
+  northWestCorner_latitude: z.number().gte(-90).lte(90),
+  northWestCorner_longitude: z.number().gte(-180).lte(180),
+
+  southEastCorner_latitude: z.number().gte(-90).lte(90),
+  southEastCorner_longitude: z.number().gte(-180).lte(180)
+})
+
+export type MapCreate = z.TypeOf<typeof mapCreateSchema>
